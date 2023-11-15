de81392e2c09c21b57d9d8a89e4749b462834ad2
Yuri Paiva <149324858+Yclpaiva@users.noreply.github.com>
2023-11-15 20:49:00 -0300

    Add files via upload

---
+++ pessoa.py
@@ -0,0 +1,13 @@
+class Pessoa:
+    def __init__(self,name,birthday,contact):
+        self.name = name
+        self.birthday = birthday
+        self.contact = contact
+    
+    def get_name(self):
+        return self.name
+    def get_birthday(self):
+        return self.birthday
+    def get_contact(self):
+        return self.contact
+    
